923a442872ae98881311e834ae4c05321f39a77b
Adeel Nasir <adeel.nasir@myant.ca>
2023-02-05 23:13:20 +0000

    some fixes

--- Sources/BrowsrLib/BrowsrLib.swift
+++ Sources/BrowsrLib/BrowsrLib.swift
@@ -1,12 +1,13 @@
 public typealias Organization = API.Types.Response.Organization
+
 public struct BrowsrLib {
     public init(completion: @escaping ([API.Types.Response.Organization]) -> Void) {
         fetchResults { (array) in
-        let sortedArray = array.sorted(by: { $0.login < $1.login })
-        completion(sortedArray)
+            let sortedArray = array.sorted(by: { $0.login < $1.login })
+            completion(sortedArray)
         }
-      }
-   public func fetchResults(completion: @escaping ([API.Types.Response.Organization]) -> Void){
+    }
+    public func fetchResults(completion: @escaping ([API.Types.Response.Organization]) -> Void){
         
         API.Client.shared
             .get(.search) { (result: Result<[API.Types.Response.Organization], API.Types.Error>) in
